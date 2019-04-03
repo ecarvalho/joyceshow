e384b20a45ff18ed3421255027d8c39ed6eb6ee4
Edson Carvalho <edsonlcj@gmail.com>
2019-04-03 19:40:24 +0200

    deploy

--- static/js/payload.js
+++ static/js/payload.js
@@ -1,15 +1,3 @@
-/*************************************************
-1. Start of use strict
-2. Smooth Scroll
-3. Logo Slider
-4. Testimonial slider
-5. Portfolio Slider
-6. MagnificPopup
-7. Shrink Header
-8. Auto close navbar-collapse on click a
-9. ctivate scrollspy to add active class to navbar items on scroll
-10. Jarallax
-*************************************************/
 (function ($) {
   // Start of use strict
   "use strict";
@@ -18,6 +6,15 @@
   * Smooth Scroll
   ------------------------------------------------*/
   $(window).on('load', function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 150) {
+      $(".trans-header").addClass("sticky");
+    } else {
+      $(".trans-header").removeClass("sticky");
+    }
+
+    $('.carousel').carousel();
+    
     jQuery('a[href*="#"]')
       // Remove links that don't actually link to anything
       .not('[href="#"]')
@@ -38,108 +35,23 @@
             // Only prevent default if animation is actually gonna happen
             event.preventDefault();
             jQuery('html, body').animate({
-              scrollTop: target.offset().top - 75
-            }, 1000, function () {
+              scrollTop: target.offset().top - 50
+            }, 800, function () {
               // Callback after animation
               // Must change focus!
 
-              var $target = jQuery(target);
-              $target.focus();
-              if ($target.is(":focus")) { // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              };
+              // var $target = jQuery(target);
+              // $target.focus();
+              // if ($target.is(":focus")) { // Checking if the target was focused
+              //   return false;
+              // } else {
+              //   $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+              //   $target.focus(); // Set focus again
+              // };
             });
           }
         }
       });
-
-    /*------------------------------------------------
-    * Logo Slider
-    ------------------------------------------------*/
-    $('.logo-slider').slick({
-      slidesToShow: 6,
-      slidesToScroll: 3,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      infinite: true,
-      dots: false,
-      arrows: false,
-      responsive: [
-        {
-          breakpoint: 1240,
-          settings: {
-            slidesToShow: 5,
-            slidesToScroll: 5,
-          }
-        },
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 5,
-            slidesToScroll: 5,
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3
-          }
-        },
-        {
-          breakpoint: 492,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 345,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
-
-    /*------------------------------------------------
-        * Testimonial slider
-    ------------------------------------------------*/
-    $('.testimonial').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: false,
-      autoplaySpeed: 2000,
-      infinite: true,
-      dots: true,
-      arrows: false,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          }
-        },
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
 
     /*------------------------------------------------
     * Portfolio Slider
@@ -167,38 +79,11 @@
     });
 
     /*------------------------------------------------
-    * MagnificPopup
-    ------------------------------------------------*/
-    $('.portfolio-gallery').magnificPopup({
-      delegate: 'a',
-      type: 'image',
-      closeOnContentClick: false,
-      closeBtnInside: false,
-      mainClass: 'mfp-with-zoom mfp-img-mobile',
-      image: {
-        verticalFit: true,
-        titleSrc: function (item) {
-          return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.el.attr('data-source') + '" target="_blank">image source</a>';
-        }
-      },
-      gallery: {
-        enabled: true
-      },
-      zoom: {
-        enabled: true,
-        duration: 300, // don't foget to change the duration also in CSS
-        opener: function (element) {
-          return element.find('img');
-        }
-      }
-    });
-
-    /*------------------------------------------------
     * Shrink Header
     ------------------------------------------------*/
     $(window).on('scroll', function () {
       var scroll = $(window).scrollTop();
-      if (scroll >= 500) {
+      if (scroll >= 150) {
         $(".trans-header").addClass("sticky");
       } else {
         $(".trans-header").removeClass("sticky");
@@ -217,7 +102,7 @@
     ------------------------------------------------*/
     $('body').scrollspy({
       target: '#main',
-      offset: 54
+      offset: 100
     });
 
     /*------------------------------------------------
